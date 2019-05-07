a7a8c45bc643ef7dbcf0506cee500fb0ebc50a19
zhups <1194286917@qq.com>
2019-05-07 10:09:36 +0800

    更新权限管理

--- page/jurisdiction/group/group.js
+++ page/jurisdiction/group/group.js
@@ -5,40 +5,34 @@ new Vue({
     el: '#app',
     data: {
         modal: false,
-        username: '',
-        pwd: '',
-        adminUserList: []
+        groupname: '',
+        content: '',
+        admingroup: []
     },
     mounted() {
-        this.getAdminUsers()
+        this.getadmingroup()
     },
     methods: {
+        gojtSet(groupId) {
+            window.location.href = 'jtSet/jtSet.html?groupId=' + groupId
+        },
         addMember() {
             this.modal = true
         },
         cancel() {
             this.modal = false
         },
-        getAdminUsers() {
+        getadmingroup() {
             let that = this
             app.requests({
-                url: 'admin/getAdminUsers',
-                data: {},
+                url: 'admin/getadmingroup',
                 success(res) {
-                    that.adminUserList = that.disadminUserList(res.data)
+                    that.admingroup = res.data
                 },
                 Error(code) {
 
                 }
             })
-        },
-        disadminUserList(data = []) {
-            let arr = data,
-                len = arr.length
-            for (let i = 0; i < len; i++) {
-                arr[i].stypeText = this.getstype(arr[i].stype)
-            }
-            return arr
         },
         getstype(n) {
             let text = ''
@@ -52,73 +46,40 @@ new Vue({
             }
             return text
         },
-        verdict(type) {
-            if (this.username == '') {
-                showToast({
-                    text: '用户名不能为空'
-                })
-                return false
-            }
-            if (this.pwd.length < 6 & this.pwd.length > 0) {
-                showToast({
-                    text: '密码必须大于6个字'
-                })
-                return false
-            }
-            if (this.pwd.length > 16) {
-                showToast({
-                    text: '密码必须小于16个字'
-                })
-                return false
-            }
-            if (type == '') {
-                showToast({
-                    text: '请选择管理员类型'
-                })
-                return false
-            }
-            return true
-        },
         submit() {
             let that = this
-            let type = document.querySelector('#selection').getAttribute('data-id') || '';
-            if (!that.verdict(type)) return
+            if (!that.groupname) {
+                showToast({
+                    text: '请输入组名称'
+                })
+                return
+            }
             app.requests({
-                url: 'admin/addadmin',
+                url: 'admin/addpermissionsgroup',
                 data: {
-                    admin_name: that.username,
-                    passwd: that.pwd,
-                    stype: type
+                    group_name: that.groupname,
+                    content: that.content
                 },
                 success(res) {
                     showToast({
                         type: 'success',
                         text: '添加成功'
                     })
-                    that.username = ''
-                    that.pwd = ''
-                    that.modal = false
-                    that.getAdminUsers()
+                    that.group_name = ''
+                    that.content = ''
+                    that.modal = ''
+                    that.getadmingroup()
                 },
                 Error(code) {
                     let text = ''
                     switch (parseInt(code)) {
                         case 3001:
-                            text = '账号不能为空'
+                            text = '分组名称错误'
                             break;
                         case 3002:
-                            text = '密码必须为6-16个字'
+                            text = '没有权限'
                             break;
                         case 3003:
-                            text = '只有root账户可以添加超级管理员'
-                            break;
-                        case 3004:
-                            text = '该账号已存在'
-                            break;
-                        case 3005:
-                            text = '没有操作权限'
-                            break;
-                        case 3006:
                             text = '添加失败'
                             break;
                         default:
--- page/jurisdiction/group/jtSet/jtSet.js
+++ page/jurisdiction/group/jtSet/jtSet.js
@@ -1,218 +1,119 @@
 import { app } from '../../../../index.js';
-import { showToast } from '../../../../js/utils.js';
+import { showToast, geturl } from '../../../../js/utils.js';
 
 new Vue({
     el: '#app',
     data: {
-        menuList: [{
-            name: '权限管理',
-            child: [{
-                name: '组管理',
-                status: 1,
-                child: [{
-                    id: 12,
-                    status: 0,
-                    name: '权限设置'
-                }, {
-                    id: 12,
-                    status: 0,
-                    name: '添加组人员'
-                }, {
-                    id: 12,
-                    status: 0,
-                    name: '删除组人员'
-                }]
-            }, {
-                name: '成员管理',
-                status: 1,
-                child: [{
-                    id: 12,
-                    status: 0,
-                    name: '添加成员'
-                }]
-            }]
-        }, {
-            name: '金额管理',
-            child: [{
-                name: '充值管理',
-                status: 1,
-                child: [{
-                    id: 12,
-                    status: 0,
-                    name: '添加充值'
-                }, {
-                    id: 12,
-                    status: 0,
-                    name: '审核充值'
-                }]
-            }, {
-                name: '开通boss',
-                status: 1,
-                child: [{
-                    id: 12,
-                    status: 0,
-                    name: '审核go'
-                }]
-            }]
-        }],
-        modal: false,
-        username: '',
-        pwd: '',
-        adminUserList: []
+        groupId: '',
+        menuList: []
     },
-    mounted() {},
+    mounted() {
+        this.groupId = geturl().groupId
+        this.getpermissionslist(this.groupId)
+    },
     methods: {
-        addMember() {
-            this.modal = true
-        },
-        cancel() {
-            this.modal = false
-        },
         selFun(k, k1, k2) {
-            let status = this.menuList[k].child[k1].child[k2].status
+            let status = this.menuList[k]._child[k1].child[k2].status
             if (status === 1) {
-                this.menuList[k].child[k1].child[k2].status = 0
+                this.menuList[k]._child[k1].child[k2].status = 0
             } else {
-                this.menuList[k].child[k1].child[k2].status = 1
+                if (this.menuList[k]._child[k1].status == 0) return
+                this.menuList[k]._child[k1].child[k2].status = 1
             }
         },
         showMenu(k, k1) {
-            let status = this.menuList[k].child[k1].status
+            let status = this.menuList[k]._child[k1].status
             if (status === 1) {
-                this.menuList[k].child[k1].status = 0
+                this.menuList[k]._child[k1].status = 0
             } else {
-                this.menuList[k].child[k1].status = 1
+                this.menuList[k]._child[k1].status = 1
             }
         },
-        getAdminUsers() {
+        getpermissionslist(group_id = '') {
             let that = this
             app.requests({
-                url: 'admin/getAdminUsers',
-                data: {},
+                url: 'admin/getpermissionslist',
+                data: {
+                    group_id: group_id
+                },
                 success(res) {
-                    that.adminUserList = that.disadminUserList(res.data)
+                    that.menuList = res.data
                 },
                 Error(code) {
 
                 }
             })
         },
-        disadminUserList(data = []) {
-            let arr = data,
-                len = arr.length
-            for (let i = 0; i < len; i++) {
-                arr[i].stypeText = this.getstype(arr[i].stype)
-            }
-            return arr
-        },
-        getstype(n) {
-            let text = ''
-            switch (parseInt(n)) {
-                case 1:
-                    text = '管理员'
-                    break;
-                case 2:
-                    text = '超级管理员'
-                    break;
-            }
-            return text
-        },
-        verdict(type) {
-            if (this.username == '') {
-                showToast({
-                    text: '用户名不能为空'
-                })
-                return false
-            }
-            if (this.pwd.length < 6 & this.pwd.length > 0) {
-                showToast({
-                    text: '密码必须大于6个字'
-                })
-                return false
-            }
-            if (this.pwd.length > 16) {
-                showToast({
-                    text: '密码必须小于16个字'
-                })
-                return false
-            }
-            if (type == '') {
-                showToast({
-                    text: '请选择管理员类型'
-                })
-                return false
-            }
-            return true
-        },
-        submit() {
-            let that = this
-            let type = document.querySelector('#selection').getAttribute('data-id') || '';
-            if (!that.verdict(type)) return
+        addpermissionsgrouppower() {
+            let that = this,
+                permissions = JSON.stringify(this.dismenuList());
             app.requests({
-                url: 'admin/addadmin',
+                url: 'admin/addpermissionsgrouppower',
                 data: {
-                    admin_name: that.username,
-                    passwd: that.pwd,
-                    stype: type
+                    group_id: that.groupId,
+                    permissions: permissions
                 },
                 success(res) {
                     showToast({
                         type: 'success',
-                        text: '添加成功'
+                        text: '保存成功'
                     })
-                    that.username = ''
-                    that.pwd = ''
-                    that.modal = false
-                    that.getAdminUsers()
+                    that.getpermissionslist(that.groupId)
                 },
                 Error(code) {
                     let text = ''
                     switch (parseInt(code)) {
                         case 3001:
-                            text = '账号不能为空'
+                            text = '分组错误'
                             break;
                         case 3002:
-                            text = '密码必须为6-16个字'
+                            text = '没有权限'
                             break;
                         case 3003:
-                            text = '只有root账户可以添加超级管理员'
+                            text = '权限分组不存在'
                             break;
                         case 3004:
-                            text = '该账号已存在'
+                            text = '权限分组不能为空'
                             break;
                         case 3005:
-                            text = '没有操作权限'
+                            text = '数据有误'
                             break;
                         case 3006:
-                            text = '添加失败'
+                            text = '菜单不存在'
+                            break;
+                        case 3007:
+                            text = '更改失败'
                             break;
                         default:
                             text = '意料之外的错误'
                     }
                     showToast({
-                        type: 'error',
                         text: text
                     })
                 }
             })
-        }
+        },
+        dismenuList() {
+            let list = this.menuList,
+                permissions = {},
+                len = list.length,
+                len1 = 0,
+                len2 = 0;
+            for (let i = 0; i < len; i++) {
+                len1 = list[i]._child.length
+                for (let x = 0; x < len1; x++) {
+                    if (list[i]._child[x].status == 1) {
+                        permissions[list[i]._child[x].id] = {}
+                        len2 = list[i]._child[x].child.length
+                        for (let y = 0; y < len2; y++) {
+                            permissions[list[i]._child[x].id][list[i]._child[x].child[y].id] = list[i]._child[x].child[y].status
+                        }
+                    }
+                }
+            }
+            return permissions
+        },
     }
-})
-select({
-el: '.combobox',
-data: [{
-id: 1,
-type_name: '管理员'
-}, {
-id: 2,
-type_name: '超级管理员'
-}]
-})
-})
-}
-})
-}
-}
 })
 select({
     el: '.combobox',
